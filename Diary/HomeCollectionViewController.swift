5ae269a36c7ca628ba50b7b8612f3fa10aba3bbe
stone56 <you@example.com>
2015-08-12 00:00:49 +0800

    Diary版本

---
+++ Diary/HomeCollectionViewController.swift
@@ -0,0 +1,103 @@
+//
+//  HomeCollectionViewController.swift
+//  Diary
+//
+//  Created by kevinzhow on 15/5/19.
+//  Copyright (c) 2015年 kevinzhow. All rights reserved.
+//
+
+import UIKit
+
+let itemHeight:CGFloat = 150.0 // Cell 的高度
+let itemWidth:CGFloat = 60 // Cell 的宽度
+let collectionViewWidth = itemWidth * 3 //同时显示三个 Cell 时候
+
+class HomeCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var yearLayout = DiaryLayout()
+        
+        yearLayout.scrollDirection = UICollectionViewScrollDirection.Horizontal
+        self.collectionView?.setCollectionViewLayout(yearLayout, animated: false)
+        self.collectionView!.frame = CGRect(x:0, y:0, width: collectionViewWidth, height: itemHeight)
+        self.collectionView!.center = CGPoint(x: self.view.frame.size.width/2.0, y: self.view.frame.size.height/2.0)
+
+        self.view.backgroundColor = UIColor.whiteColor()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        //#warning Incomplete method implementation -- Return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //#warning Incomplete method implementation -- Return the number of items in the section
+        return 1
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> HomeYearCollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("HomeYearCollectionViewCell", forIndexPath: indexPath) as! HomeYearCollectionViewCell
+        
+        cell.textInt = 2015
+        cell.labelText = "二零一五 年"
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        var leftRightMagrin = (collectionViewWidth - itemWidth)/2
+        return UIEdgeInsetsMake(0, leftRightMagrin, 0, leftRightMagrin);
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
+    
+    }
+    */
+
+}
---
+++ Diary/Label/DiaryLabel.swift
@@ -0,0 +1,68 @@
+//
+//  DiaryLabel.swift
+//  Diary
+//
+//  Created by kevinzhow on 15/5/19.
+//  Copyright (c) 2015年 kevinzhow. All rights reserved.
+//
+
+import UIKit
+
+class DiaryLabel: UILabel {
+    
+    var textAttributes: [NSObject : AnyObject]!
+    
+    convenience init(fontname:String ,labelText:String, fontSize : CGFloat, lineHeight: CGFloat){
+        
+        self.init(frame: CGRectZero)
+        
+        let font = UIFont(name: fontname, size: fontSize) as UIFont!
+        
+        var paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = lineHeight
+        
+        textAttributes = [NSFontAttributeName: font, NSParagraphStyleAttributeName: paragraphStyle]
+        
+        var labelSize = sizeHeightWithText(labelText, fontSize ,textAttributes)
+        
+        self.frame = CGRectMake(0, 0, labelSize.width, labelSize.height)
+        
+        self.attributedText = NSAttributedString(string: labelText, attributes: textAttributes)
+        self.lineBreakMode = NSLineBreakMode.ByCharWrapping
+        self.numberOfLines = 0
+    }
+    
+    func resizeLabelWithFontName(fontname:String, labelText:String, fontSize : CGFloat, lineHeight: CGFloat ){
+        let font = UIFont(name: fontname, size: fontSize) as UIFont!
+        
+        var paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = lineHeight
+        
+        textAttributes = [NSFontAttributeName: font, NSForegroundColorAttributeName: UIColor.blackColor(),  NSParagraphStyleAttributeName: paragraphStyle]
+        var labelSize = sizeHeightWithText(labelText, fontSize ,textAttributes)
+        
+        self.frame = CGRectMake(0, 0, labelSize.width, labelSize.height)
+        
+        self.attributedText = NSAttributedString(string: labelText, attributes: textAttributes)
+        self.lineBreakMode = NSLineBreakMode.ByCharWrapping
+        self.numberOfLines = 0
+    }
+    
+    func updateText(labelText: String) {
+        
+        var labelSize = sizeHeightWithText(labelText, self.font.pointSize ,textAttributes)
+        self.frame = CGRectMake(0, 0, labelSize.width, labelSize.height)
+        self.attributedText = NSAttributedString(string: labelText, attributes: textAttributes)
+    }
+    
+    func updateLabelColor(color: UIColor) {
+        textAttributes[NSForegroundColorAttributeName] = color
+        self.attributedText = NSAttributedString(string: self.attributedText.string, attributes: textAttributes)
+    }
+    
+}
+
+func sizeHeightWithText(labelText: NSString, fontSize: CGFloat, textAttributes: [NSObject : AnyObject]) -> CGRect {
+    
+    return labelText.boundingRectWithSize(CGSizeMake(fontSize, 480), options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: textAttributes, context: nil)
+}
